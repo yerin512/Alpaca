c0462fd53f8c1ed8c2eb44c1ef24634a33f4001e
kyeong-one <corvax93@gmail.com>
2020-11-12 17:47:41 +0900

    sql 폴더 생성 및 텍스트 파일 생성

---
+++ trip/src/main/java/com/myteam/trip/plan/controller/PlanController.java
@@ -0,0 +1,11 @@
+package com.myteam.trip.plan.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class PlanController {
+	@RequestMapping(value = "/location", method = RequestMethod.GET)
+	public String location() {
+	    return "/location";
+	}
+}
