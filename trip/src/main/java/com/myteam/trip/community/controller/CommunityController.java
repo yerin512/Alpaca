f0f87bab2b467ff3d718ce676089c4a4223a60ff
yerin <73337811+yerin512@users.noreply.github.com>
2020-11-19 00:03:07 +0900

    머쓱니티

--- trip/src/main/java/com/myteam/trip/community/controller/CommunityController.java
+++ trip/src/main/java/com/myteam/trip/community/controller/CommunityController.java
@@ -1,13 +1,22 @@
 package com.myteam.trip.community.controller;
 
+import java.io.File;
+import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -15,9 +24,10 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.myteam.trip.community.service.CommunityService;
 import com.myteam.trip.community.vo.CommunityVO;
+import com.myteam.trip.member.vo.MemberVO;
 
  
-
+@Repository
 @Controller("communityController")
 public class CommunityController{
 	private static final String CURR_IMAGE_REPO_PATH = "C:\\community\\article_image";
@@ -39,6 +49,65 @@ public class CommunityController{
 			return mav; 
 	}
 
+	// 한 개 이미지 글쓰기
+	@RequestMapping(value = "/community/communityForm.do", method = RequestMethod.POST)
+	public ResponseEntity communityForm(MultipartHttpServletRequest multipartRequest, HttpServletResponse response)
+			throws Exception {
+		multipartRequest.setCharacterEncoding("utf-8");
+		Map<String, Object> communityMap = new HashMap<String, Object>();
+		Enumeration enu = multipartRequest.getParameterNames();
+		while (enu.hasMoreElements()) {
+			String name = (String) enu.nextElement();
+			String value = multipartRequest.getParameter(name);
+			communityMap.put(name, value);
+		}
+
+		List<String> imageFileName = upload(multipartRequest); 
+		HttpSession session = multipartRequest.getSession();
+		MemberVO memberVO = (MemberVO) session.getAttribute("member"); 
+		String id = memberVO.getId();
+		communityMap.put("id", id);
+		communityMap.put("imageFileName", imageFileName);
+
+		String message;
+		ResponseEntity resEnt = null;
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.add("Content-Type", "text/html; charset=utf-8");
+		try {
+			int c_no = communityService.addNewCommunity(communityMap);
+			if (imageFileName != null) {
+				for (int i = 0; i < imageFileName.size(); i++) {
+					File srcFile = new File(COMMUNITY_IMAGE_REPO + "\\" + "temp" + "\\" + imageFileName.get(i));
+					File destDir = new File(COMMUNITY_IMAGE_REPO + "\\" + c_no);
+					FileUtils.moveFileToDirectory(srcFile, destDir, true);
+				}
+			}
+
+			message = "<script>";
+			message += " alert('새글을 추가했습니다.');";
+			message += " location.href='" + multipartRequest.getContextPath() + "/board/listArticles.do'; ";
+			message += " </script>";
+			resEnt = new ResponseEntity(message, responseHeaders, HttpStatus.CREATED);
+		} catch (Exception e) {
+			File srcFile = new File(COMMUNITY_IMAGE_REPO + "\\" + "temp" + "\\" + imageFileName);
+			srcFile.delete();
+
+			message = " <script>";
+			message += " alert('오류가 발생했습니다. 다시 시도해 주세요');');";
+			message += " location.href='" + multipartRequest.getContextPath() + "/community/communityForm.do'; ";
+			message += " </script>";
+			resEnt = new ResponseEntity(message, responseHeaders, HttpStatus.CREATED);
+			e.printStackTrace();
+		}
+		return resEnt;
+	}	
+	
+	
+
+	private List<String> upload(MultipartHttpServletRequest multipartRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	public ResponseEntity addNewCommunity(MultipartHttpServletRequest multipartRequest, HttpServletResponse response)
 			throws Exception {
--- trip/src/main/java/com/myteam/trip/community/vo/CommunityVO.java
+++ trip/src/main/java/com/myteam/trip/community/vo/CommunityVO.java
@@ -2,16 +2,85 @@ package com.myteam.trip.community.vo;
 
 import java.sql.Date;
 
+import org.springframework.stereotype.Component;
+
+
+@Component("communityVO")
 public class CommunityVO {
 	
 	private int c_no;
 	private String c_title;
 	private String c_content;
 	private String c_imageFileName;
-	private String id;
 	private Date c_date;
+	private String id;
 	
-	public void CommunityVO() {}
+	public CommunityVO() {
+		
+	}
+	
+
+	public CommunityVO(int c_no, String c_title, String c_content, String c_imageFileName, Date c_date, String id) {
+		super();
+		this.c_no = c_no;
+		this.c_title = c_title;
+		this.c_content = c_content;
+		this.c_imageFileName = c_imageFileName;
+		this.c_date = c_date;
+		this.id = id;
+	}
+
+
+
+	public int getC_no() {
+		return c_no;
+	}
+
+	public void setC_no(int c_no) {
+		this.c_no = c_no;
+	}
+
+	public String getC_title() {
+		return c_title;
+	}
+
+	public void setC_title(String c_title) {
+		this.c_title = c_title;
+	}
+
+	public String getC_content() {
+		return c_content;
+	}
+
+	public void setC_content(String c_content) {
+		this.c_content = c_content;
+	}
+
+	public String getC_imageFileName() {
+		return c_imageFileName;
+	}
+
+	public void setC_imageFileName(String c_imageFileName) {
+		this.c_imageFileName = c_imageFileName;
+	}
+
+	public Date getC_date() {
+		return c_date;
+	}
+
+	public void setC_date(Date c_date) {
+		this.c_date = c_date;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
 	
 
 }
