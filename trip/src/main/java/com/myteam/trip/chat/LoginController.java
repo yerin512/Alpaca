220ddf7c85299e082ac2d62b7eafe934eaff226d
dionysus0214 <hiba0214@naver.com>
2020-11-28 13:25:10 +0900

    예린씨가 어젯밤에 올린 거랑 병합하면서 채팅 쪽 에러뜨는 거 수정 반영

--- trip/src/main/java/com/myteam/trip/chat/LoginController.java
+++ trip/src/main/java/com/myteam/trip/chat/LoginController.java
@@ -12,17 +12,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class LoginController {
 
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/chat/login.do", method = RequestMethod.GET)
 	private ModelAndView main(HttpServletRequest request, HttpServletResponse response) {
 		String viewName = (String)request.getAttribute("viewName");
@@ -31,9 +25,6 @@ public class LoginController {
 		return mav;
 	}
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/chat/loginProcess.do", method = RequestMethod.POST)
 	public String loginProcess(@RequestParam String id, HttpServletRequest request) {
 
