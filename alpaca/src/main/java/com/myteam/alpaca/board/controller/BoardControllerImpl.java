364b56fba71a179a0405518e6c53c29ea4fcfeed
KIM-I <kaipi10hou@gmail.com>
2020-12-02 17:15:49 +0900

    자잘수정

--- alpaca/src/main/java/com/myteam/alpaca/board/controller/BoardControllerImpl.java
+++ alpaca/src/main/java/com/myteam/alpaca/board/controller/BoardControllerImpl.java
@@ -239,7 +239,7 @@ public class BoardControllerImpl implements BoardController {
 
 			message = " <script>";
 			message += " alert('작성에 실패란 없다');";
-			message += " location.href='" + multipartRequest.getContextPath() + "/board/articleForm.do'; ";
+			message += " location.href='" + multipartRequest.getContextPath() + "/board/listArticles.do'; ";
 			message += " </script>";
 			resEnt = new ResponseEntity(message, responseHeaders, HttpStatus.CREATED);
 			e.printStackTrace();
